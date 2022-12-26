aa9a216f8c4835142d89d45a11193cba3a6871f0
Hasan <ha03330224926@gmail.com>
2022-12-26 19:12:12 +0500

    Remove the DOM-Tutorial Folder

--- DOM-Tutorial/getThe_Diemensionof_element.js
+++
@@ -1,8 +0,0 @@
-// How to get the diemension of elements
-
-// height width
-
-const sectiontodo = document.querySelector(".section-todo");
-const info = sectiontodo.getBoundingClientRect();  // gives the all information of section-todo class 
-console.log(info);
-
--- DOM-Tutorial/insertAdjacent_HTML.js
+++
@@ -1,9 +0,0 @@
-// elem.insertAdjacentHTML(where, html)
-// beforebegin
-// afterbegin
-// beforeend
-// afterend
-
-const todoList = document.querySelector(".todo-list");
-todoList.insertAdjacentHTML("Afterbegin", "<li>Teach Student </li>");
-
